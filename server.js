d1a9d7386971b06683dde436dff1d077f9be9044
ViniciusDev <viniciusdev10@gmail.com>
2025-01-21 11:04:56 -0300

    created conncetion with database mysql

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const userRoute = require('./routes/userRoute')
 
+const db = require('./models/db')
+
 app.use(express.json())
 const PORT = 3033
 
