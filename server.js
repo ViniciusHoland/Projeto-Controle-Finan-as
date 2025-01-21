4873eaa8ba829a19b995528fd0585a45a93ebe37
ViniciusDev <viniciusdev10@gmail.com>
2025-01-21 08:51:53 -0300

    project created

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+const PORT = 3033
+
+app.listen(PORT, () => {
+
+    console.log('project running on', PORT)
+
+})
